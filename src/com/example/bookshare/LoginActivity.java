b9c09fe5e3625b7e503faa5f5e1eb9dfa5763670
Qero <laiqing3321568@qq.com>
2014-03-26 18:46:26 +0800

    登录界面前期

--- src/com/example/bookshare/LoginActivity.java
+++ src/com/example/bookshare/LoginActivity.java
@@ -20,55 +20,24 @@ import android.widget.TextView;
 public class LoginActivity extends Activity {
 
 	private TextView username,password;
-	private WebView web;
-	public  String[] m_basename = { "ACM", "数模", "机器人" };
-	private int now_flag;//当前下拉框的下标
-	private Spinner m_Spinner;
-	private ArrayAdapter<String> adapter;
-	private RelativeLayout mainLayout;
+	
+	
 
 	@SuppressLint("NewApi")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
-		/*if(Build.VERSION.SDK_INT >= 11) {
-		      StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder().detectDiskReads     ().detectDiskWrites().detectNetwork().penaltyLog().build());
-		   StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder().detectLeakedSqlLiteObjects().detectLeakedClosableObjects().penaltyLog().penaltyDeath().build());
-		  }*/
 		
 		username = (TextView) findViewById(R.id.USERNAME);
 		password = (TextView) findViewById(R.id.PASSWORD);
-		
-	 
-		mainLayout = (RelativeLayout) findViewById(R.id.mainLayout);
-		//mainLayout.setBackgroundColor(Color.argb(150, 22, 70, 150));
-		
-
-		
-		m_Spinner = (Spinner) findViewById(R.id.BASENAME);
-
-		adapter = new ArrayAdapter<String>(this,
-				android.R.layout.simple_spinner_dropdown_item, m_basename);// 连接可选内容
-
-		m_Spinner.setAdapter(adapter);// 设置下拉框
-		m_Spinner.setOnItemSelectedListener(new Spinner.OnItemSelectedListener(){
-			public void onItemSelected(AdapterView<?> arg0,View arg1,int arg2,long arg3)
-			{
-				now_flag = arg2;
-			}
-
-			@Override
-			public void onNothingSelected(AdapterView<?> arg0) {
-				// TODO Auto-generated method stub
-				
-			}
-		});
 		
 	}
 
 	public void Login(View v)// 登录相应按钮
 	{
+		
+		
 		
 		Intent intent = new Intent();
 		intent.setClass(this, MainActivity.class);
