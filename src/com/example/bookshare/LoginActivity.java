695ea2b105752883162148261c05387c0d212f8c
Qero <laiqing3321568@qq.com>
2014-03-27 00:10:54 +0800

    UI线程和子线程通信

--- src/com/example/bookshare/LoginActivity.java
+++ src/com/example/bookshare/LoginActivity.java
@@ -1,49 +1,64 @@
 package com.example.bookshare;
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.HttpParams;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.StrictMode;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
-import android.webkit.WebView;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.EditText;
-import android.widget.RelativeLayout;
-import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends Activity {
 
-	private TextView username,password;
-	
-	
-
 	@SuppressLint("NewApi")
-	@Override
+	private Handler mMainHandler, mChildHandler;
+	ChildThread test;
+
+	@SuppressLint("HandlerLeak")
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
+
+		mMainHandler = new Handler() {
+			public void handleMessage(Message msg) {
+				Log.i("hello","我是主线程，我屌爆了子线程的屎忽窿！");
+			}
+		};
 		
-		username = (TextView) findViewById(R.id.USERNAME);
-		password = (TextView) findViewById(R.id.PASSWORD);
-		
+		new ChildThread().start();
+
 	}
 
-	public void Login(View v)// 登录相应按钮
-	{
+	public void Login(View v) throws JSONException, ClientProtocolException,
+			IOException {
+		String username, password;
+		username = ((TextView) findViewById(R.id.USERNAME)).getText()
+				.toString();
+		password = ((TextView) findViewById(R.id.PASSWORD)).getText()
+				.toString();
 		
-		
-		
-		Intent intent = new Intent();
-		intent.setClass(this, MainActivity.class);
-		startActivity(intent);
-	
+		mChildHandler.sendEmptyMessage(100);
 	}
 
 	public void Register(View v)// 登录相应按钮
@@ -60,5 +75,33 @@ public class LoginActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+	
+	class ChildThread extends Thread {
+		 
+	    private static final String CHILD_TAG = "ChildThread";
+
+	    public void run() {
+	        this.setName("ChildThread");
+
+	        //初始化消息循环队列，需要在Handler创建之前
+	        Looper.prepare();
+
+	        mChildHandler = new Handler() {
+	        	public void handleMessage(Message msg){
+	        		Log.i("hello","我是子线程，我被主线程屌爆了屎忽窿！");
+	        		mMainHandler.sendEmptyMessage(100);
+	        		}
+	        	};
+	        	Log.i(CHILD_TAG, "Child handler is bound to - "+ mChildHandler.getLooper().getThread().getName());
+	        	//启动子线程消息循环队列
+	        	Looper.loop();
+	    }
+	}
+
+	public void onDestroy() {
+		super.onDestroy();
+		mChildHandler.getLooper().quit();
+		}
 
 }
+
