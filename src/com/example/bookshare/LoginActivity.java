046e3752f1940b72e4920600c86f734c2e093bd0
toufu <465253100@qq.com>
2014-03-27 16:19:06 +0800

    初步实现登录

--- src/com/example/bookshare/LoginActivity.java
+++ src/com/example/bookshare/LoginActivity.java
@@ -1,20 +1,19 @@
 package com.example.bookshare;
 
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.params.BasicHttpParams;
-import org.apache.http.params.HttpConnectionParams;
-import org.apache.http.params.HttpParams;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
 import org.json.JSONException;
-import org.json.JSONObject;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
@@ -27,7 +26,6 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class LoginActivity extends Activity {
 
@@ -42,10 +40,10 @@ public class LoginActivity extends Activity {
 
 		mMainHandler = new Handler() {
 			public void handleMessage(Message msg) {
-				Log.i("hello","我是主线程，我屌爆了子线程的屎忽窿！");
+				Log.i("hello", "我是主线程，我屌爆了子线程的屎忽窿！");
 			}
 		};
-		
+
 		new ChildThread().start();
 
 	}
@@ -57,8 +55,14 @@ public class LoginActivity extends Activity {
 				.toString();
 		password = ((TextView) findViewById(R.id.PASSWORD)).getText()
 				.toString();
-		
-		mChildHandler.sendEmptyMessage(100);
+
+		Message msg = new Message();
+		Bundle data = new Bundle();
+		data.putString("username", username);
+		data.putString("password", password);
+		msg.setData(data);
+		msg.what = 0x1234;
+		mChildHandler.sendMessage(msg);
 	}
 
 	public void Register(View v)// 登录相应按钮
@@ -75,33 +79,68 @@ public class LoginActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-	
+
 	class ChildThread extends Thread {
-		 
-	    private static final String CHILD_TAG = "ChildThread";
 
-	    public void run() {
-	        this.setName("ChildThread");
+		private static final String CHILD_TAG = "ChildThread";
 
-	        //初始化消息循环队列，需要在Handler创建之前
-	        Looper.prepare();
+		public void run() {
+			this.setName("ChildThread");
 
-	        mChildHandler = new Handler() {
-	        	public void handleMessage(Message msg){
-	        		Log.i("hello","我是子线程，我被主线程屌爆了屎忽窿！");
-	        		mMainHandler.sendEmptyMessage(100);
-	        		}
-	        	};
-	        	Log.i(CHILD_TAG, "Child handler is bound to - "+ mChildHandler.getLooper().getThread().getName());
-	        	//启动子线程消息循环队列
-	        	Looper.loop();
-	    }
+			// 初始化消息循环队列，需要在Handler创建之前
+			Looper.prepare();
+
+			mChildHandler = new Handler() {
+				public void handleMessage(Message msg) {
+					if (msg.what == 0x1234) {
+						Bundle data = msg.getData();
+						String username = data.getString("username");
+						String password = data.getString("password");
+
+						Log.i("username", username);
+						Log.i("password", password);
+
+						HttpClient httpLogin = new DefaultHttpClient();
+						HttpResponse loginResponse = null;
+						HttpPost post = new HttpPost(
+								"http://192.168.1.10:8080/BookShareYii/index.php?r=user/mbsignin");
+						List<NameValuePair> nvps = new ArrayList<NameValuePair>();
+						nvps.add(new BasicNameValuePair("LoginForm[username]",
+								username));
+						nvps.add(new BasicNameValuePair("LoginForm[password]",
+								password));
+						try {
+							post.setEntity(new UrlEncodedFormEntity(nvps,
+									"UTF-8"));
+							loginResponse = httpLogin.execute(post);
+							String strMsg = EntityUtils.toString(loginResponse
+									.getEntity());
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+
+						/*
+						 * Message loginResponseMsg = new Message(); Bundle
+						 * bundle = new Bundle();
+						 * bundle.putString("loginResponse", strMsg);
+						 * loginResponseMsg.setData(bundle);
+						 * mMainHandler.sendMessage(loginResponseMsg);
+						 */
+
+					}
+				}
+
+			};
+			Log.i(CHILD_TAG, "Child handler is bound to - "
+					+ mChildHandler.getLooper().getThread().getName());
+			// 启动子线程消息循环队列
+			Looper.loop();
+		}
 	}
 
 	public void onDestroy() {
 		super.onDestroy();
 		mChildHandler.getLooper().quit();
-		}
+	}
 
 }
-
