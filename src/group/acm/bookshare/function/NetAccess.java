340210d96855eb444849d276a77f00afbf5ad042
Qero <laiqing3321568@qq.com>
2014-08-30 14:30:54 +0800

    add the function about communicate by SSL

--- src/group/acm/bookshare/function/NetAccess.java
+++ src/group/acm/bookshare/function/NetAccess.java
@@ -3,15 +3,29 @@ package group.acm.bookshare.function;
 import group.acm.bookshare.util.Utils;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.Socket;
 import java.net.URL;
+import java.net.UnknownHostException;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
 import java.util.List;
 import java.util.zip.GZIPInputStream;
 
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
+import org.apache.http.HttpVersion;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
@@ -20,8 +34,18 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpPut;
 import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.conn.ClientConnectionManager;
+import org.apache.http.conn.params.ConnManagerParams;
+import org.apache.http.conn.scheme.PlainSocketFactory;
+import org.apache.http.conn.scheme.Scheme;
+import org.apache.http.conn.scheme.SchemeRegistry;
+import org.apache.http.conn.ssl.SSLSocketFactory;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.params.CoreConnectionPNames;
+import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.HttpParams;
+import org.apache.http.params.HttpProtocolParams;
 import org.apache.http.protocol.HTTP;
 import org.apache.http.util.EntityUtils;
 
@@ -49,10 +73,106 @@ public class NetAccess {
 	private static NetAccess internetaccess = new NetAccess();
 
 	private NetAccess() {
-		httpClient = new DefaultHttpClient();
+		KeyStore trustStore;
+		try {
+			trustStore = KeyStore.getInstance(KeyStore.getDefaultType());
+			trustStore.load(null, null);
+
+	         SSLSocketFactory sf = new SSLSocketFactoryEx(trustStore);
+	         sf.setHostnameVerifier(SSLSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);  //允许所有主机的验证
+
+	         HttpParams params = new BasicHttpParams();
+
+	         HttpProtocolParams.setVersion(params, HttpVersion.HTTP_1_1);
+	         HttpProtocolParams.setContentCharset(params,
+	                 HTTP.DEFAULT_CONTENT_CHARSET);
+	         HttpProtocolParams.setUseExpectContinue(params, true);
+
+	         // 设置连接管理器的超时
+	         ConnManagerParams.setTimeout(params, 10000);
+	         // 设置连接超时
+	         HttpConnectionParams.setConnectionTimeout(params, 10000);
+	         // 设置socket超时
+	         HttpConnectionParams.setSoTimeout(params, 10000);
+
+	         // 设置http https支持
+	         SchemeRegistry schReg = new SchemeRegistry();
+	         schReg.register(new Scheme("http", PlainSocketFactory
+	                 .getSocketFactory(), 80));
+	         schReg.register(new Scheme("https", sf, 443));
+
+	         ClientConnectionManager conManager = new ThreadSafeClientConnManager(params, schReg);
+
+	        this.httpClient = new DefaultHttpClient(conManager, params);
+			
+		} catch (KeyStoreException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (CertificateException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (KeyManagementException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UnrecoverableKeyException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		/*httpClient = new DefaultHttpClient();
 		httpClient.getParams().setParameter(
-				CoreConnectionPNames.CONNECTION_TIMEOUT, 5000);
+				CoreConnectionPNames.CONNECTION_TIMEOUT, 5000);*/
 	}
+	
+	class SSLSocketFactoryEx extends SSLSocketFactory {
+		 
+	    SSLContext sslContext = SSLContext.getInstance("TLS");
+	 
+	    public SSLSocketFactoryEx(KeyStore truststore)
+	            throws NoSuchAlgorithmException, KeyManagementException,
+	            KeyStoreException, UnrecoverableKeyException {
+	        super(truststore);
+	 
+	        TrustManager tm = new X509TrustManager() {
+	 
+	            @Override
+	            public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+	                return null;
+	            }
+	 
+	            @Override
+	            public void checkClientTrusted(
+	                    java.security.cert.X509Certificate[] chain, String authType)
+	                    throws java.security.cert.CertificateException {
+	 
+	            }
+	 
+	            @Override
+	            public void checkServerTrusted(
+	                    java.security.cert.X509Certificate[] chain, String authType)
+	                    throws java.security.cert.CertificateException {
+	 
+	            }
+	        };
+	 
+	        sslContext.init(null, new TrustManager[] { tm }, null);
+	    }
+	    @Override  
+	    public Socket createSocket(Socket socket, String host, int port, boolean autoClose) throws IOException, UnknownHostException {  
+	        return sslContext.getSocketFactory().createSocket(socket, host, port, autoClose);  
+	    }  
+	  
+	    @Override  
+	    public Socket createSocket() throws IOException {  
+	        return sslContext.getSocketFactory().createSocket();  
+	    }  
+	}
+	
 
 	// 获取实例对象的唯一方法
 	public static NetAccess getInstance() {
